04836e17398bdd0e093fea69f9a2648f6cd77b1b
lokalmatador123 <38790188+lokalmatador123@users.noreply.github.com>
2022-01-08 18:28:26 +0100

    Update A_create_project.py

--- apscale/A_create_project.py
+++ apscale/A_create_project.py
@@ -13,15 +13,15 @@ def create_project(project_name):
         return None
 
     ## generate the subfolder structure
-    subfolders = ['1_raw data\data',
-                  '2_demultiplexing\data',
-                  '3_PE_merging\data',
-                  '4_primer_trimming\data',
-                  '5_quality_filtering\data',
-                  '6_dereplication_pooling\data\dereplication',
-                  '6_dereplication_pooling\data\pooling',
-                  '7_otu_clustering\data',
-                  '8_denoising\data']
+    subfolders = ['1_raw data/data',
+                  '2_demultiplexing/data',
+                  '3_PE_merging/data',
+                  '4_primer_trimming/data',
+                  '5_quality_filtering/data',
+                  '6_dereplication_pooling/data/dereplication',
+                  '6_dereplication_pooling/data/pooling',
+                  '7_otu_clustering/data',
+                  '8_denoising/data']
 
     subfolders = [Path(project_name).joinpath(subfolder) for subfolder in subfolders]
 
@@ -38,45 +38,43 @@ def create_project(project_name):
     del wb['Sheet']
 
     ## write the 3_PE_merging sheet
-    df_3 = pd.DataFrame([[psutil.cpu_count() - 2, 25, 199, 5]],
-                        columns = ['cores to use', 'maxdiffpct', 'maxdiffs', 'minovlen'])
+    df_0 = pd.DataFrame([[psutil.cpu_count() - 2, 6]],
+                        columns = ['cores to use', 'compression level'])
+
+    df_0.to_excel(writer, sheet_name = '0_general_settings', index = False)
+
+    ## write the 3_PE_merging sheet
+    df_3 = pd.DataFrame([[25, 199, 5]],
+                        columns = ['maxdiffpct', 'maxdiffs', 'minovlen'])
 
     df_3.to_excel(writer, sheet_name = '3_PE_merging', index = False)
 
     ## write the 4_primer_trimming sheet
-    df_4 = pd.DataFrame([[psutil.cpu_count() - 2, '', '', 'False']],
-                        columns = ['cores to use', "P5 Primer (5' - 3')", "P7 Primer (5' - 3')", 'anchoring'])
+    df_4 = pd.DataFrame([['', '', 'False']],
+                        columns = ["P5 Primer (5' - 3')", "P7 Primer (5' - 3')", 'anchoring'])
 
     df_4.to_excel(writer, sheet_name = '4_primer_trimming', index = False)
 
     ## write the 5_quality_filtering sheet
-    df_5 = pd.DataFrame([[psutil.cpu_count() - 2, 1, '', '']],
-                        columns = ['cores to use', "maxEE", "min length", 'max length'])
+    df_5 = pd.DataFrame([[1, '', '']],
+                        columns = ["maxEE", "min length", 'max length'])
 
     df_5.to_excel(writer, sheet_name = '5_quality_filtering', index = False)
 
-    ## write the 6_dereplication_pooling sheet
-    df_6 = pd.DataFrame([[psutil.cpu_count() - 2]],
-                        columns = ['cores to use'])
-
-    df_6.to_excel(writer, sheet_name = '6_dereplication_pooling', index = False)
-
     ## write the 7_otu_clustering sheet
-    df_7 = pd.DataFrame([[psutil.cpu_count() - 2, 97]],
-                        columns = ['cores to use', 'pct id'])
+    df_7 = pd.DataFrame([[97]],
+                        columns = ['pct id'])
 
     df_7.to_excel(writer, sheet_name = '7_otu_clustering', index = False)
 
     ## write the 8_denoising sheet
-    df_8 = pd.DataFrame([[psutil.cpu_count() - 2, 2, 8]],
-                        columns = ['cores to use', 'alpha', 'minsize'])
+    df_8 = pd.DataFrame([[2, 8]],
+                        columns = ['alpha', 'minsize'])
 
     df_8.to_excel(writer, sheet_name = '8_denoising', index = False)
-
-
 
     ## save the Settings file againg
     wb.save(Path(project_name).joinpath('Settings.xlsx'))
     writer.close()
 
-create_project('HiSeq Testrun')
+create_project('C:/Users/dommi/Desktop/Development')
