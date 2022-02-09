3e40d87e5dd876ca69beba93fbd77124b8b7806f
DominikBuchner <38790188+DominikBuchner@users.noreply.github.com>
2022-02-09 08:05:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="apscale",
-    version="1.3.2",
+    version="1.3.3",
     author="Dominik Buchner",
     author_email="dominik.buchner524@googlemail.com",
     description="Advanced Pipeline for Simple yet Comprehensive AnaLysEs of DNA metabarcoding data",
