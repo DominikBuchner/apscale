9bd1899ce89f572b60472859f5045bdd086c25a1
DominikBuchner <38790188+DominikBuchner@users.noreply.github.com>
2024-08-07 17:01:51 +0200

    update

--- apscale/f_denoising_v2.py
+++ apscale/f_denoising_v2.py
@@ -1,7 +1,8 @@
-import subprocess, datetime, gzip, os, pickle, glob, openpyxl, shutil, psutil, re, sys
+import subprocess, datetime, gzip, os, pickle, glob, openpyxl, shutil, psutil, re, sys, hashlib
 import pandas as pd
 import numpy as np
 from joblib import Parallel, delayed
+from Bio import SeqIO
 from pathlib import Path
 from Bio.SeqIO.FastaIO import SimpleFastaParser
 from io import StringIO
@@ -18,12 +19,12 @@ def denoise(file, project=None, comp_lvl=None, alpha=None, minsize=None):
     sample_name_out_1 = "{}_ESVs_with_chimeras.fasta.gz".format(
         Path(file).with_suffix("").with_suffix("").name
     )
-    output_path = Path(project).joinpath("7_denoising", "data", sample_name_out_1)
+    output_path_1 = Path(project).joinpath("7_denoising", "data", sample_name_out_1)
 
     ## run vsearch --cluster_unoise to denoise data to ESV level
     ## use --log because for some reason no info is written to stderr with this command
     ## write stdout to uncompressed output at runtime
-    with open(output_path.with_suffix(""), "w") as output:
+    with open(output_path_1.with_suffix(""), "w") as output:
         f = subprocess.run(
             [
                 "vsearch",
@@ -52,11 +53,11 @@ def denoise(file, project=None, comp_lvl=None, alpha=None, minsize=None):
         )
 
     ## compress the output, remove uncompressed output
-    with open(output_path.with_suffix(""), "rb") as in_stream, gzip.open(
-        output_path, "wb", comp_lvl
+    with open(output_path_1.with_suffix(""), "rb") as in_stream, gzip.open(
+        output_path_1, "wb", comp_lvl
     ) as out_stream:
         shutil.copyfileobj(in_stream, out_stream)
-    os.remove(output_path.with_suffix(""))
+    os.remove(output_path_1.with_suffix(""))
 
     ## collect processed and passed reads from the log file
     with open(
@@ -78,6 +79,96 @@ def denoise(file, project=None, comp_lvl=None, alpha=None, minsize=None):
             datetime.datetime.now().strftime("%H:%M:%S"), sample_name_out_1, seqs, esvs
         )
     )
+
+    # exchange the names in the output path
+    output_path_2 = Path(project).joinpath(
+        "7_denoising",
+        "data",
+        sample_name_out_1.replace("_with_chimeras", "").replace(
+            "PE_trimmed_filtered_dereplicated_", ""
+        ),
+    )
+
+    ## run vsearch --uchime_denovo to remove chimeric sequences from the ESVs
+    with open(output_path_2.with_suffix(""), "w") as output:
+        f = subprocess.run(
+            [
+                "vsearch",
+                "--uchime_denovo",
+                output_path_1,
+                "--relabel",
+                "ESV_",
+                "--nonchimeras",
+                "-",
+                "-fasta_width",
+                str(0),
+                "--quiet",
+            ],
+            stdout=output,
+            stderr=subprocess.DEVNULL,
+        )
+
+    ## compress the output, remove uncompressed output
+    with open(output_path_2.with_suffix(""), "rb") as in_stream, gzip.open(
+        output_path_2, "wb", comp_lvl
+    ) as out_stream:
+        shutil.copyfileobj(in_stream, out_stream)
+    os.remove(output_path_2.with_suffix(""))
+
+    # remove the files including chimeras afterwards
+    os.remove(Path(project).joinpath("7_denoising", "data", sample_name_out_1))
+
+    ## collect processed and passed reads from the output fasta, since it is not reported in the log
+    f = list(SimpleFastaParser(gzip.open(output_path_2, "rt")))
+
+    # give user output
+    print(
+        "{}: {}: {} chimeras removed from {} ESV sequences.".format(
+            datetime.datetime.now().strftime("%H:%M:%S"),
+            output_path_2.with_suffix("").with_suffix("").name,
+            int(esvs) - len(f),
+            esvs,
+        )
+    )
+    print(
+        "{}: ESVs saved to {}.".format(
+            datetime.datetime.now().strftime("%H:%M:%S"),
+            output_path_2,
+        )
+    )
+
+
+def calculate_hash_headers(file, project=None, comp_lvl=None):
+    """Function to transform sequence headers to sha3 256 hashes to easily compare ESV sequences."""
+    ## define the name for the output fasta
+    ## create an output path to write to
+    sample_name_out_1 = "{}_ESVs_with_chimeras.fasta.gz".format(
+        Path(file).with_suffix("").with_suffix("").name
+    )
+
+    # exchange the names in the output path
+    output_path_2 = Path(project).joinpath(
+        "7_denoising",
+        "data",
+        sample_name_out_1.replace("_with_chimeras", "").replace(
+            "PE_trimmed_filtered_dereplicated_", ""
+        ),
+    )
+
+    # open the fasta file to a dict
+    fasta_data = SimpleFastaParser(gzip.open(output_path_2, "rt"))
+
+    # generate the final output path
+    output_path_3 = Path(str(output_path_2).replace("_ESVs", "_ESV_hash"))
+
+    # write the output
+    with gzip.open(output_path_3, "wt+") as out_stream:
+        for _, seq in fasta_data:
+            header = seq.encode("ascii")
+            header = hashlib.sha3_256(header).hexdigest()
+            out_stream.write(">{}\n{}\n".format(header, seq))
+
+    os.remove(output_path_2)
 
 
 ## main function for the denoising script
@@ -132,6 +223,19 @@ def main(project=Path.cwd()):
         delayed(denoise)(
             file, project=project, comp_lvl=comp_lvl, alpha=alpha, minsize=minsize
         )
+        for file in input
+    )
+
+    ## give user output
+    print(
+        "{}: Constructing final ESV fasta files.".format(
+            datetime.datetime.now().strftime("%H:%M:%S")
+        )
+    )
+
+    ## parallelize the denoising
+    Parallel(n_jobs=cores)(
+        delayed(calculate_hash_headers)(file, project=project, comp_lvl=comp_lvl)
         for file in input
     )
 
