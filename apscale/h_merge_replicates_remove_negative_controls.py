8f8d800c91e18146828f93214ba7509f3e0fb4b9
DominikBuchner <38790188+DominikBuchner@users.noreply.github.com>
2025-02-19 13:35:09 +0100

    update

--- apscale/h_merge_replicates_remove_negative_controls.py
+++ apscale/h_merge_replicates_remove_negative_controls.py
@@ -6,7 +6,7 @@ import numpy as np
 
 def merge_replicates(
     esv_table: object, replicate_del: str, minimum_presence: int
-) -> object:
+) -> tuple:
     """Function to merge replicates based on the replicate delimiter and a minimum number of occurences
 
     Args:
@@ -15,7 +15,7 @@ def merge_replicates(
         mininum_presence (int): Minimum number of replicates with reads present to accept as true signal (e.g. 2)
 
     Returns:
-        object: Returns a pandas dataframe with merge replicates.
+        tuple: Returns a pandas dataframe with merged replicates and a dataframe with merging stats.
     """
     # preserve sequences and unique ids
     seqs, unique_ids = esv_table.pop("Seq"), esv_table.pop("unique_ID")
@@ -86,16 +86,16 @@ def merge_replicates(
             retained_reads = 100
 
         if pre_merged_esvs != 0:
-            retained_esvs = ((pre_merged_esvs - post_merged_esvs) * 100 / pre_merged_esvs)
+            retained_esvs = (pre_merged_esvs - post_merged_esvs) * 100 / pre_merged_esvs
         else:
             retained_esvs = 100
         # give user output
         print(
-            "{}: {}: {:.2f} % of reads removed. {:.2f} % of ESVs removed".format(
+            "{}: {}: {:.2f} % of reads removed. {:.2f} % of ESVs removed.".format(
                 datetime.datetime.now().strftime("%H:%M:%S"),
                 sample_name,
                 retained_reads,
-                retained_esvs
+                retained_esvs,
             )
         )
 
@@ -113,6 +113,92 @@ def merge_replicates(
 
     return merged_table, merging_stats
 
+
+def substract_neg_controls(esv_table: object, nc_prefix: str) -> tuple:
+    """Function to substract the maximum read in all negative controls.
+
+    Args:
+        esv_table (object): ESV table as pandas dataframe.
+        nc_prefix (str): Prefix to detect the negative controls (e.g. NC_)
+
+    Returns:
+        tuple: Returns the ESV table with removed negative controls and the stats for removal.
+    """
+    # extract the negative controls from the esv table
+    ncs = pd.concat(
+        [esv_table.pop(col) for col in esv_table.columns if col.startswith(nc_prefix)],
+        axis=1,
+    )
+    # calculate the maximum number of reads in the negative controls
+    ncs = ncs.max(axis=1)
+    nc_rem_stats = []
+
+    # give user output
+    for esv, nc_count in zip(esv_table["temporary_ID"], ncs):
+        if nc_count > 0:
+            print(
+                "{}: {} reads substracted from {}.".format(
+                    datetime.datetime.now().strftime("%H:%M:%S"), nc_count, esv
+                )
+            )
+            nc_rem_stats.append((esv, nc_count))
+
+    # generate the nc removal stats table
+    nc_rem_stats = pd.DataFrame(data=nc_rem_stats, columns=["esv", "substracted reads"])
+
+    # update the esv table
+    esv_table = pd.concat(
+        [
+            esv_table.iloc[:, :2],
+            esv_table.iloc[:, 2:-1].sub(ncs, axis=0).clip(0),
+            esv_table.iloc[:, -1],
+        ],
+        axis=1,
+    )
+
+    # return updated esv table and statistics for project report
+    return esv_table, nc_rem_stats
+
+
+def remove_empty_rows(esv_table: object) -> object:
+    """Function to remove empty rows from esv table after merging or negative control substraction
+
+    Args:
+        esv_table (object): Dataframe holding the esv table.
+
+    Returns:
+        object: Dataframe with empty rows removed
+    """
+    pre_esv_count = len(esv_table.index)
+    esv_table["readsum"] = esv_table.iloc[:, 2:-1].sum(axis=1)
+    esv_table = esv_table.loc[esv_table["readsum"] > 0]
+    esv_table_clean = esv_table.drop("readsum", axis=1)
+
+    print(
+        "{}: {} empty lines removed from ESV table.".format(
+            datetime.datetime.now().strftime("%H:%M:%S"),
+            pre_esv_count - len(esv_table_clean.index),
+        )
+    )
+
+    return esv_table_clean
+
+
+def save_outputs(esv_table: object, project:str, merging_stats=None, nc_rem_stats=None) -> None:
+    """Function to write all output of the merging and negative removal step.
+
+    Args:
+        esv_table (object): Dataframe holding the updated ESV table.
+        project (str): Path to the apscale project
+        merging_stats (object, optional): Dataframe with the merging statistics. Defaults to None.
+        nc_rem_stats (object, optional): Dataframe with the negative control removal statistics. Defaults to None.
+    """
+    # write the updated esv table as parquet and excel
+
+    # write the updated fasta file
+
+    # add the merging and nc stats to the project report
+    pass
 
 def main(project=Path.cwd()) -> None:
     """Main function to merge replicates and remove negative controls from the dataset
@@ -156,12 +242,30 @@ def main(project=Path.cwd()) -> None:
             )
         )
 
-        merged_table, merging_stats = merge_replicates(
+        esv_table, merging_stats = merge_replicates(
             esv_table, replicate_del, minimum_presence
         )
 
     # remove negative controls afterward (optional)
+    if substract_ncs:
+        # give user output
+        print(
+            "{}: Substracting reads found in negative controls.".format(
+                datetime.datetime.now().strftime("%H:%M:%S")
+            )
+        )
+
+        esv_table, nc_rem_stats = substract_neg_controls(esv_table, nc_prefix)
 
     # remove empty rows and columns from the esv table
+    if replicate_merging or substract_ncs:
+        esv_table = remove_empty_rows(esv_table)
 
-    # write outputs and project report
+        # write outputs and project report
+
+    else:
+        print(
+            "{}: Replicate merging and negative control substraction disabled. Skipping step.".format(
+                datetime.datetime.now().strftime("%H:%M:%S")
+            )
+        )
