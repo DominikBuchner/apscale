6883927faa13b72d7303d8bcb8e053f63973c570
DominikBuchner <38790188+DominikBuchner@users.noreply.github.com>
2025-07-03 15:29:22 +0200

    update

--- apscale/pages/6_Export_read_tables.py
+++ apscale/pages/6_Export_read_tables.py
@@ -107,12 +107,48 @@ def filter_sequence_metadata(read_data_to_modify: object) -> object:
         return sequence_metadata
 
 
-def create_data_split(read_data_to_modify):
-    sample_metadata = dd.read_hdf(
-        read_data_to_modify, key="sample_metadata"
-    ).columns.to_list()
-    print(sample_metadata)
-    pass
+def create_data_split(read_data_to_modify: object) -> list:
+    sample_metadata = dd.read_hdf(read_data_to_modify, key="sample_metadata")
+
+    columns_to_select = sample_metadata.select_dtypes(
+        include=["object", "string"]
+    ).columns
+
+    help = """This option will only display columns that do not hold numeric data, as splitting on those will result
+    in one read table per sample. Leave empty if a full read table is needed."""
+
+    levels_to_split = st.multiselect(
+        label="Columns to split read table on:", options=columns_to_select, help=help
+    )
+
+    # collect the sample filters here
+    if levels_to_split:
+        unique_rows = sample_metadata[levels_to_split].drop_duplicates().compute()
+        sample_splits = unique_rows.to_dict(orient="records")
+        return sample_splits
+    else:
+        return []
+
+
+def export_read_tables(
+    read_data_to_modify: str,
+    sequence_metadata: object,
+    sample_split: list,
+    metadata_to_export: list,
+    sequence_type: str,
+    output_format: str,
+):
+    """Function to export read tables with the selected filters split by the selected sample metadata.
+
+    Args:
+        read_data_to_modify (str): Path to the read storage.
+        sequence_metadata (object): Dask dataframe that holds the (filtered) sequence metadata
+        sample_split (list): List of dictionarys for filter to apply when splitting output frames by sample metadata.
+        metadata_to_export (list): Sequence metadata columns to export.
+        sequence_type (str): Wether to export sequences or sequence groups.
+        output_format (str): String that defines the output format
+    """
+    print(read_data_to_modify)
 
 
 def main():
@@ -144,7 +180,7 @@ def main():
     st.divider()
 
     st.header("Split output tables by sample metadata")
-    data_split = create_data_split(st.session_state["read_data_to_modify"])
+    sample_splits = create_data_split(st.session_state["read_data_to_modify"])
 
     st.divider()
     # let the user decide the output format
@@ -167,7 +203,26 @@ def main():
         horizontal=True,
         key="sequence_type",
     )
+
+    # sequence metadata to include in output table
+    metadata_to_export = st.multiselect(
+        label="Select sequence metadata to include in the output table",
+        options=sequence_metadata.columns,
+        default=sequence_metadata.columns,
+    )
+
     export = st.button(label="Export read tables", type="primary")
+
+    if export:
+        # export read tables in the apscale analyze folder
+        export_read_tables(
+            st.session_state["read_data_to_modify"],
+            sequence_metadata,
+            sample_splits,
+            metadata_to_export,
+            output_sequence_type,
+            output_file_format,
+        )
 
 
 main()
